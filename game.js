3ccc4594352feb791b1a4f3b5462727be5c741b0
Aaron Wilson <aaronwil28@gmail.com>
2016-12-05 10:42:13 -0800

    commit finish

--- game.js
+++ game.js
@@ -1,22 +1,23 @@
-
-
-
-
-
 var player1 = document.getElementById("cat")
 var  food1 = document.getElementById("honey")
 var xpos = 200
- var ypos = 25
- var width = 90
-  var height = 90
-  var foodEaten = 0
-
+var ypos = 25
+var width = 90
+var height = 90
+var foodEaten = 0
+var timeStart = Date.now();
+var gameOver;
 
    playerX = Number(player1.getAttribute("x"))
    playerY = Number(player1.getAttribute("y"))
 
    foodX = Number(food1.getAttribute("x"))
    foodY = Number(food1.getAttribute("y"))
+
+   function randomNumber(min,max)
+   {
+    return Math.floor(Math.random()*(max-min+1)+min);
+   }
 
    if (playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + height) {
      var randX = randomNumber(200,600)
@@ -53,23 +54,19 @@ document.addEventListener("keydown", function (e) {
     food1.setAttribute("x", randX)
   }
 
+if (playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + height){
+  foodEaten = foodEaten + 1
+  document.getElementById("Score").textContent = foodEaten
+}
 
-if (foodEaten == 5) {
- document.getElementById("Screen").pauseAnimation
+
+if (foodEaten == 3) {
+ document.getElementById("gameboard").pauseAnimations()
  var timeStop = Date.now()
  var timeDuration = timeStop - timeStart
  timeDuration = timeDuration/1000
- document.getElementById("totaltime").textContent
+ document.getElementById("totaltime").textContent = timeDuration
+ document.getElementById("Score").textContent = foodEaten
 
 }
- document.getElementById("Score").textContent = foodEaten
- // randomNumber returns a random number between min and max
- function randomNumber(min,max)
- {
-  return Math.floor(Math.random()*(max-min+1)+min);
- }
-
-
-
-
 })
